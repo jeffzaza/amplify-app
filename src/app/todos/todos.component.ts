522e6dc1c6db03090bce8753595862fba7abbf2c
EC2 Default User <ec2-user@ip-172-31-0-202.ap-southeast-1.compute.internal>
2025-07-22 03:35:32 +0000

    in the mid of implementing todos

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { generateClient, type Client } from 'aws-amplify/api';
+import { Todo, ListTodosQuery } from '../../API';
+import * as mutations from '../../graphql/mutations';
 
 @Component({
+  standalone: true,
+  imports: [ReactiveFormsModule],
   selector: 'app-todos',
-  imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
+  public createForm: FormGroup;
+  public client: Client;
 
+  constructor(private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    this.client = generateClient();
+  }
+
+  public async onCreate(todo: Todo) {
+    try {
+      const response = await this.client.graphql({
+        query: mutations.createTodo,
+        variables: {
+          input: todo
+        }
+      });
+      console.log('item created!', response);
+      this.createForm.reset();
+    } catch (e) {
+      console.log('error creating todo...', e);
+    }
+  }
 }
