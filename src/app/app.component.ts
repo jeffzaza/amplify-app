43323c5e8ca1e998bb2991e5ad70a806d114ff0c
EC2 Default User <ec2-user@ip-172-31-0-202.ap-southeast-1.compute.internal>
2025-07-22 06:16:28 +0000

    Add the login form and style into the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TodosComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TodosComponent,
+    AmplifyAuthenticatorModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'amplify-app';
+  title = 'amplifyapp';
 }
