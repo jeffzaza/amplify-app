0ad8e7d551bd914ce6f1e9f62e587e3c712a0bf5
EC2 Default User <ec2-user@ip-172-31-0-202.ap-southeast-1.compute.internal>
2025-07-22 05:49:35 +0000

    initially add amplify auth

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators
 } from '@angular/forms';
+
 import { generateClient, type Client } from 'aws-amplify/api';
 import { Todo, ListTodosQuery } from '../../API';
 import * as mutations from '../../graphql/mutations';
+import * as queries from '../../graphql/queries';
+import * as subscriptions from '../../graphql/subscriptions';
 
 @Component({
   standalone: true,
-  imports: [ReactiveFormsModule],
   selector: 'app-todos',
+  imports: [ReactiveFormsModule],
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit, OnDestroy {
+  public todos: ListTodosQuery['listTodos'];
   public createForm: FormGroup;
   public client: Client;
+
+  private subscription: any = null;
 
   constructor(private fb: FormBuilder) {
     this.createForm = this.fb.group({
@@ -27,6 +33,38 @@ export class TodosComponent {
     });
 
     this.client = generateClient();
+  }
+
+  async ngOnInit() {
+    /* fetch todos when app loads */
+    try {
+      const response = await this.client.graphql({
+        query: queries.listTodos
+      });
+      this.todos = response.data.listTodos;
+    } catch (e) {
+      console.log('error fetching todos', e);
+    }
+
+    this.subscription = this.client
+      .graphql({
+        query: subscriptions.onCreateTodo
+      })
+      .subscribe({
+        next: (event: any) => {
+          const newTodo: Todo = event.data.onCreateTodo;
+          if (this.todos) {
+            this.todos.items = [newTodo, ...this.todos.items];
+          }
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = null;
   }
 
   public async onCreate(todo: Todo) {
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
 import { Amplify } from 'aws-amplify';
 import amplifyconfig from './amplifyconfiguration.json';
 Amplify.configure(amplifyconfig);
